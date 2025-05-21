4807bad5bb17c5ec658ff6a67a010b7f93f5c652
Junnot Daniel <jerenasonjunnot@gmail.com>
2025-05-21 20:17:42 +0300

    Add function to perform addition of two numbers

---
+++ week02/add_numbers.py
@@ -0,0 +1,10 @@
+def add(first, second):
+    """Add the values and returns the result
+
+    Args:
+        first (number): first number
+        second (number): second number
+    """
+
+    result = first + second
+    return result
